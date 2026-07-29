18a48a734366223afa63e0b5a4ce8d018fa6ced2
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-07-30 01:34:35 +0600

    update

--- django1/dj1/dj1/urls.py
+++ django1/dj1/dj1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myProject project.
+URL configuration for dj1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', home, name='home')
 ]
