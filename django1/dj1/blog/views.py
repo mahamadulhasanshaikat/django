ce9cb3cb9553f3494a2cf9ba9bda55f9c9f30c79
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2026-07-31 22:23:45 +0600

    views and urls

--- django1/dj1/blog/views.py
+++ django1/dj1/blog/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to the blog home page")
+
+def about(request):
+    a=10+50
+    return HttpResponse(f"This is about page: {a}")
